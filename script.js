60537a455d1fafd5f754eda0ef0228ae5cef6faf
Rudy Brandies <adanb82@gmail.com>
2023-09-20 13:31:10 -0500

    added sort and total functions

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let data = [];
 async function getRandomUser() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
-
     const user = data.results[0];
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
@@ -20,6 +19,53 @@ async function getRandomUser() {
     addData(newUser);
 }
 
+function doubleMoney() {
+    data = data.map((user) => {
+        return {...user, money: user.money * 2}
+    });
+
+    updateDOM();
+}
+
+function sortByRichest() {
+    data.sort((a,b) => b.money - a.money);
+    updateDOM();
+}
+
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+    updateDOM();
+}
+
+function calculatWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 function addData(obj) {
     data.push(obj);
+    updateDOM();
 }
+
+function updateDOM(providedData = data) {
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+     providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+     });
+}
+
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculatWealth);
+  
